78c0cc727c06faf214645186043518fbf71ede3f
AGANG <1343557727@qq.com>
2026-05-06 17:39:00 +0800

    搞定加载界面

--- DanmakuDesigner/Application.xaml.vb
+++ DanmakuDesigner/Application.xaml.vb
@@ -1,6 +1,21 @@
-﻿Class Application
+﻿Imports ResourcePack
+Class Application
+    Private LW As LoadWindow
+    Private Async Sub Application_Startup(sender As Object, e As StartupEventArgs)
+        LW = New LoadWindow
+        LW.Show()
+        Await LoadResource()
+        Dim mw As New MainWindow
+        mw.Show()
+        LW.Close()
+    End Sub
 
     ' Application-level events, such as Startup, Exit, and DispatcherUnhandledException
     ' can be handled in this file.
-
+    Private Async Function LoadResource() As Task
+        Await Task.Run(Sub()
+                           Textures.Load()
+                           Sounds.Load()
+                       End Sub)
+    End Function
 End Class
